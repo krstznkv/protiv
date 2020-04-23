144592562feed2e8fda6795249e1f3f1334d2872
krstznkv <milkyyy.wayyyy@gmail.com>
2020-04-24 01:58:00 +0400

    ticket

--- demo/src/main/java/com/example/demo/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<User, Long> {
-}
