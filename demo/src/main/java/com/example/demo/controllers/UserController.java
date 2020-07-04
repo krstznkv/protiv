bbd2ad6621bce3d48ce3e70b230cdb46c3ff8378
krstznkv <milkyyy.wayyyy@gmail.com>
2020-07-04 13:39:30 +0400

    на всякий

--- demo/src/main/java/com/example/demo/controllers/UserController.java
+++ demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -9,31 +9,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
-    private final  UserService userService;
-
-    @GetMapping("test")
-    public ResponseEntity<String> test() {
-        return ResponseEntity.ok("hello");
-    }
+    private final UserService userService;
 
     @PostMapping("registration")
     public ResponseEntity<User> registration(@RequestBody User user) {
-       return ResponseEntity.ok(userService.save(user));
+        return ResponseEntity.ok(userService.save(user));
     }
+
     @PostMapping("registrationA")
     public ResponseEntity<User> registrationA(@RequestBody User user) {
         user.setRole(Role.ROLE_ADMIN);
         return ResponseEntity.ok(userService.save(user));
     }
 
+    @PostMapping("/findUser")
+    public ResponseEntity<Message> findUser(@RequestBody String username) {
+        User u = (User) userService.loadUserByUsername(username);
+        if(u==null) return  ResponseEntity.ok(new Message("not found"));
+        if (u.getRoles().contains(Role.ROLE_ADMIN))
+            return ResponseEntity.ok(new Message("ADMIN"));
+        else return ResponseEntity.ok(new Message("USER"));
+    }
+
     @GetMapping("/login")
-    public ResponseEntity<Message>  authorize()
-    {
+    public ResponseEntity<Message> authorize() {
         return ResponseEntity.ok(new Message("Success"));
     }
 }
